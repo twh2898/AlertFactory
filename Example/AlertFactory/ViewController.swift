c6c5218147d99e06296e3d141a83c650a63aab3a
Thomas Harrison <twh2898@vt.edu>
2021-08-19 16:27:45 -0400

    Added example code

--- Example/AlertFactory/ViewController.swift
+++ Example/AlertFactory/ViewController.swift
@@ -6,9 +6,12 @@
 //  Copyright (c) 2021 Thomas Harrison. All rights reserved.
 //
 
+import AlertFactory
 import UIKit
 
 class ViewController: UIViewController {
+
+    let useShorthand = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,4 +23,69 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func alertButtonAction(_ sender: Any) {
+        // Use the UIViewController extension for a single method version
+        if useShorthand {
+            self.alert(
+                title: "Alert", message: "This is an alert",
+                confirmAction: {
+                    print("Alert was confirmed")
+                })
+        } else {
+            var factory = AlertFactory(title: "Alert")
+            factory.message = "You can pass options to AlertFactory.init or set them later"
+            let alert = factory.alert(confirmAction: {
+                print("Alert was confirmed")
+            })
+            present(alert, animated: true)
+        }
+    }
+
+    @IBAction func confirmButtonAction(_ sender: Any) {
+        if useShorthand {
+            self.confirm(
+                title: "Confirm", message: "This is a confirmation",
+                confirmAction: {
+                    print("Confirm was confirmed")
+                },
+                cancelAction: {
+                    print("Confirm was canceled")
+                })
+        } else {
+            let factory = AlertFactory(title: "Confirm", message: "This is a confirmation")
+            let alert = factory.confirm(
+                confirmAction: {
+                    print("Confirm was confirmed")
+                },
+                cancelAction: {
+                    print("Confirm was canceled")
+                })
+            present(alert, animated: true)
+        }
+    }
+
+    @IBAction func promptButtonAction(_ sender: Any) {
+        if useShorthand {
+            self.prompt(
+                title: "Prompt", message: "This is a prompt", placeholder: "Placeholder",
+                confirmAction: { text in
+                    if let text = text {
+                        print("Prompt was confirmed with text", text)
+                    }
+                },
+                cancelAction: {
+                    print("Prompt was canceled")
+                })
+        } else {
+            let factory = AlertFactory(title: "Prompt", message: "This is a prompt")
+            let alert = factory.prompt(
+                confirmAction: { text in
+                    print("Prompt was confirmed with", text!)
+                },
+                cancelAction: {
+                    print("Prompt was canceled")
+                })
+            present(alert, animated: true)
+        }
+    }
 }
